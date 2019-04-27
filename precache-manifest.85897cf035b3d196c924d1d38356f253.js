10f0b8c918d28b71a1e6cab068da6f6901dd178c
Kate Cowley <kateacowley@gmail.com>
2019-04-27 16:30:06 -0700

    Updates

---
+++ precache-manifest.85897cf035b3d196c924d1d38356f253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b117f52c296186f9a0e131274b69cd07",
+    "url": "/color-match-game/index.html"
+  },
+  {
+    "revision": "4995ff439d9b0a7f6dd3",
+    "url": "/color-match-game/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "67e99389e88e5fa23331",
+    "url": "/color-match-game/static/css/main.ab0f200a.chunk.css"
+  },
+  {
+    "revision": "4995ff439d9b0a7f6dd3",
+    "url": "/color-match-game/static/js/2.5c27d705.chunk.js"
+  },
+  {
+    "revision": "67e99389e88e5fa23331",
+    "url": "/color-match-game/static/js/main.8db7a305.chunk.js"
+  },
+  {
+    "revision": "6d412181f8fd9c045772",
+    "url": "/color-match-game/static/js/runtime~main.0cf3dba2.js"
+  }
+]);
